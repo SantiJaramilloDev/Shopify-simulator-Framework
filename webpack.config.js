ef3b3b51c8cf889049c626d832a7dfa751c2b0c7
Santiago Jaramillo <santiagojaramillo2611@gmail.com>
2025-05-02 09:51:53 -0500

    Ajustes para webpack y para el server
    Se ajusta el webpack para que funcione correctamente y el server para que el proyecto corra con normalidad

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.liquid$/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
     ],
   },
   mode: 'development',
